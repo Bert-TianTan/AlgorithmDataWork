9f806e2bd3fd7dbb0be0c3ff2aa1becedb526184
Bert-TianTan <351085690@qq.com>
2021-11-02 19:47:05 +0800

    1.添加运算符对象操作

--- main.cpp
+++ main.cpp
@@ -22,8 +22,29 @@ typedef struct Money {
         return x*2.0;
     }
 
-} data;
 
+} Money_S;
+
+
+double sum2(Money_S mobj, int n, int m){
+    double result = 0;
+    for(int i = n; i<=m;i++){
+        result+= mobj(i);
+    }
+    return result;
+}
+
+
+#include <functional>
+
+template<class F> double sum(F f, int n, int m){
+
+    double result = 0;
+    for(int i = n; i<=m;i++){
+        result+= f(i);
+    }
+    return result;
+}
 
 
 
@@ -43,6 +64,18 @@ int main()
     cout << strlen(data) <<endl;
 
     print_stod_stol();
+
+
+    //运算符重写 函数对象
+    Money datas;
+    cout << "operator = " << datas(5) << endl;
+
+    cout << "operator sum2= " << sum2(datas, 2, 6) << endl;
+
+
+    cout << "sum template  = "<<sum(negate<double>(), 4, 6) << endl;
+
+
 
     return 0;
 }
